68e126c23ab79bd374779d8a9b9165eb7cbb23d3
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2020-09-08 00:12:11 -0400

    add frontend app for working with blockchain through metamask

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -14,10 +14,34 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
-    function addCandidate (string memory _name) private {
+    function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    function vote(uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
     // constructor 
     constructor() public {
